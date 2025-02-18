f9a89c865d68b8aa26c991ecc20dcf478c79d6cf
Rachele1010 <61459424+Rachele1010@users.noreply.github.com>
2025-02-18 12:56:06 +0100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,74 @@
 import streamlit as st
-import pandas as pd
-import math
-from pathlib import Path
+# Importa tutte le funzioni dalla cartella "Function"
+st.set_page_config(layout="wide", page_title="Land instability", page_icon="🌍", initial_sidebar_state="auto")
+import hydralit_components as hc
+from PIL import Image
+from correlation import correlation
+from display_dashboard import display_dashboard
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+def main():
+    # Definisci il menu con le voci principali e i sottomenu
+    menu_data = [
+        {'label': "Dashboard", 'ttip': "I'm the Dashboard tooltip!"},
+        {'label': "Correlation", 'ttip': "I'm the Correlation tooltip!"},
+        {'label': "Info", 'ttip': "For some problem, Contact us!"}
+    ]
+        # Tema personalizzato per il menu
+    over_theme = {
+        'txc_inactive': '#FFFFFF',  # Colore del testo inattivo
+        'menu_background': '#2E8B57',  # Verde scuro per lo sfondo della barra
+        'txc_active': '#FFFFFF',  # Testo bianco quando attivo
+        'option_active': '#2E8B57',  # Verde scuro menta per l'opzione attiva
+    }
+    # Carica l'immagine "itineris.jpg" per il logo in alto a sinistra
+    logo_itineris = Image.open('itineris.jpg')
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
-
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
+    # Crea la barra di navigazione al centro
+    menu_id = hc.nav_bar(
+        menu_definition=menu_data,
+        override_theme=over_theme,
+        hide_streamlit_markers=True, # Mostra il segnaposto di Streamlit
+        sticky_nav=True, # Appiccicoso in alto
+        sticky_mode='sticky', # Appiccicoso
     )
+    # Cambia l'URL in base al menu selezionato
+    if menu_id == 'Dashboard':
+        st.query_params["page"] = "Dashboard"
+    elif menu_id == 'Correlation':
+        st.query_params["page"] = "Correlation"
+    elif menu_id == 'Info':
+        st.query_params["page"] = "Info"
+    # Retrieve and display the current query parameters
+    query_params = st.query_params
+#################################################################################################################################################################################################################
+##################### Dashboard #################################################################################################################################################################################
+#################################################################################################################################################################################################################
+    # Verifica cosa è stato selezionato nel menu
+    if menu_id == "Dashboard":
+        # Carica e mostra l'immagine di copertura centrata        
+        st.title("Welcome to Downstream - Land Domain")
+        display_dashboard()
+        st.stop()
+#################################################################################################################################################################################################################
+##################### UPLOAD #################################################################################################################################################################################
+#################################################################################################################################################################################################################
+    ## Sezione di Upload
+    if menu_id == "Correlation":
+        correlation()
+#################################################################################################################################################################################################################
+##################### CONTACT #################################################################################################################################################################################
+#################################################################################################################################################################################################################
+    # Sezione Contatti
+    if menu_id == "Info":
+        st.header("Information")
+        st.subheader("Contact Us")
+        st.write("""Information about how to contact the team or get support. 
+                 Rachele Franceschini : rfranceschini@ogs.it""")
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.markdown("For more details about ITINERIS project, click on link -> **[ITINERIS](https://itineris.d4science.org/)**")
+        with col2:
+            st.image(logo_itineris, caption='Copyright © ITINERIS 2023-2024', use_container_width=False)
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+if __name__ == "__main__":
+    main()
